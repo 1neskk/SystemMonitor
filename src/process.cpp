061792c5040fd179e808f8d2514379ef63db47be
1neskk <contato.nesk@outlook.com>
2023-05-04 18:35:20 -0300

    Initial commit

---
+++ src/process.cpp
@@ -0,0 +1,58 @@
+#include "process.h"
+
+#include <unistd.h>
+
+#include <cctype>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "linux_parser.h"
+using std::string;
+using std::to_string;
+using std::vector;
+
+Process::Process(int pid)
+{
+  m_pid = pid;
+  m_command = LinuxParser::Command(pid);
+  std::string ram = LinuxParser::Ram(pid);
+  m_ram = std::stol(ram);
+  m_uptime = LinuxParser::UpTime(pid);
+  m_user = LinuxParser::User(pid);
+
+  long seconds = LinuxParser::UpTime() - m_uptime;
+  long totaltime = LinuxParser::ActiveJiffies(pid);
+  try
+  {
+    m_utilization = float(totaltime) / float(seconds);
+
+  } catch (...)
+  {
+    m_utilization = 0;
+  }
+}
+
+int Process::Pid() const { return m_pid; }
+
+
+float Process::CpuUtilization() const { return m_utilization; }
+
+
+string Process::Command() const { return m_command; }
+
+
+string Process::Ram() const { return std::to_string(m_ram); }
+
+int Process::getRam() const { return m_ram; }
+
+string Process::User() const { return m_user; }
+
+
+long int Process::UpTime() const { return m_uptime; }
+
+bool Process::operator<(Process const& a) const
+{
+  return CpuUtilization() < a.CpuUtilization();
+}
---
+++ src/system.cpp
@@ -0,0 +1,70 @@
+#include "system.h"
+
+#include <unistd.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "format.h"
+#include "linux_parser.h"
+#include "process.h"
+#include "processor.h"
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+
+Processor& System::Cpu()
+{
+  return cpu_;
+}
+
+vector<Process>& System::Processes()
+{
+  vector<int> pids = LinuxParser::Pids();
+  processes_.clear();
+  for (long unsigned int i = 0; i < pids.size(); i++) 
+  {
+    if (!LinuxParser::Ram(pids[i]).empty())
+    {
+      Process process(pids[i]);
+      processes_.push_back(process);
+    }
+  } 
+  std::sort(processes_.rbegin(), processes_.rend());
+  return processes_;
+}
+
+std::string System::Kernel()
+{
+  return LinuxParser::Kernel();
+}
+
+float System::MemoryUtilization()
+{
+  return LinuxParser::MemoryUtilization();
+}
+
+std::string System::OperatingSystem()
+{
+  return LinuxParser::OperatingSystem();
+}
+
+int System::RunningProcesses()
+{
+  return LinuxParser::RunningProcesses();
+}
+
+int System::TotalProcesses()
+{
+  return LinuxParser::TotalProcesses();
+}
+
+long int System::UpTime()
+{
+  long uptime = LinuxParser::UpTime();
+  return uptime;
+}
